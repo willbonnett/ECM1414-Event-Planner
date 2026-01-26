4265a63270f0c332bbc25f927c00d7f4534390c4
TobyAD123 <tad218@exeter.ac.uk>
2026-01-26 11:16:25 +0000

    Add files via upload

---
+++ File Handler.py
@@ -0,0 +1,36 @@
+
+def getData(file):
+    file = open(file, "r")
+
+    numActivities = int(file.readline())
+    line2 = file.readline().split()
+
+    maxTime = int(line2[0])
+    maxBudget = int(line2[1])
+
+    print("There are", numActivities, "activities.")
+    print("The maximum available time is", maxTime)
+    print("The maximum available budget is", maxBudget)
+    print("")
+
+
+    activityList = []
+
+    for i in range(numActivities):
+        nextLine = file.readline().split()
+        for i in range(len(nextLine)):
+            
+            try:
+                nextLine[i] = int(nextLine[i])
+                
+            except ValueError:
+                continue
+        
+        activityList.append(nextLine)
+
+    return activityList
+
+
+
+activityList, maxTime, maxBudget = getData("input_100.txt")
+
